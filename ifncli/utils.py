8bdc2fef6173274c2de96d4ea7e7c672be8edabf
Clément Turbelin <clement.turbelin@gmail.com>
2020-11-12 16:47:35 +0100

    add formating helpers

--- ifncli/utils.py
+++ ifncli/utils.py
@@ -8,3 +8,31 @@ def read_yaml(path):
 def read_json(path):
     data = json.load(open(path, 'r', encoding='UTF-8'))
     return data
+
+def to_json(object):
+    return json.dumps(object)
+
+def readable_yaml(object):
+    return yaml.dump(object, default_flow_style=False, sort_keys=False)
+
+# Create a list 
+# json json data
+# fields list of fields to extract
+def json_to_list(json, fields):
+    if isinstance(fields, dict):
+        ff = []
+        cols = []
+        for col,f in fields.items():
+           cols.append(col)
+           ff.append(f) 
+        fields = ff   
+    else:
+        cols = fields 
+    rows = []
+    for row in json:
+        r = []
+        for field in fields:
+            v = row[field]
+            r.append(v)
+        rows.append(r)
+    return (cols, rows)
