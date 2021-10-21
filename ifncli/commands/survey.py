1f38f7a6301e5a618410609412489d0630fd236f
Clément Turbelin <clement.turbelin@upmc.fr>
2021-10-21 12:55:46 +0200

    add response checks, expected problems in profile, output formats

--- ifncli/commands/survey.py
+++ ifncli/commands/survey.py
@@ -1,4 +1,8 @@
 from datetime import datetime
+import json
+import sys
+from ifncli.formatter import create_context
+from ifncli.formatter.readable import as_readable
 from ifncli.formatter.survey.validator import SurveyStandardValidator, ValidatorProfile
 
 from cliff.command import Command
@@ -22,6 +26,13 @@ class SurveyValidateStandard(Command):
         parser.add_argument(
             "--profile",help="path to the validation profile", required=True,
         )
+
+        parser.add_argument(
+            "--all",help="Returns all problems, including known", required=False, action="store_true"
+        )
+
+        parser.add_argument("--output", help="path of file to output results", required=False)
+        parser.add_argument("--format", help="Output format available, default is 'human' (human readable yaml-like)", required=False, action="store", default='human', choices=['human','json','yaml'])
         return parser
 
     def take_action(self, args):
@@ -36,7 +47,28 @@ class SurveyValidateStandard(Command):
 
         validator = SurveyStandardValidator(profile)
         r = validator.validate(survey.getCurrent())
+        
+        if not args.all:
+            r = validator.filter_known(r)
 
-        print(readable_yaml(r))
+        ctx = create_context()
+
+        if args.output:
+            need_close = True
+            out = open(args.output, 'w')
+        else:
+            need_close = False
+            out = sys.stdout
+
+        out_format = args.format
+
+        ss = as_readable(r, ctx)
+        if out_format in ["human", "yaml"]:
+            out.write(readable_yaml(ss))
+        else:
+            out.write(json.dumps(ss))
+
+        if need_close:
+            out.close()
 
 register(SurveyValidateStandard)
--- ifncli/formatter/survey/validator.py
+++ ifncli/formatter/survey/validator.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 import json
+from typing import Dict, List, Optional
 from ifncli.commands import survey
+from ifncli.formatter.models.dictionnary import ItemDictionnary
 from ifncli.formatter.models.survey import SurveyItem, SurveySingleItem
 from ifncli.formatter.survey import standard
 from ifncli.formatter.survey.standard.models import (MATRIX_CHOICE_TYPE, MULTIPLE_CHOICE_TYPE)
@@ -27,6 +29,36 @@ def read_from_url(url):
         else:
             raise ConfigError("read_from_url(%s): %s" % (url, e.code))
 
+class ValidatorProblem:
+
+    MISSING = 'question_missing'
+    NOT_DEFINED = 'question_unknown'
+    WRONG_TYPE = 'wrong_type'
+    MISSING_VALIDATION = 'missing_validation'
+    OPT_NOT_DEFINED = 'option_unknown'
+    OPT_MISSING = 'option_missing'
+
+    def __init__(self, type:str, name:str, expected:Optional[str]=None) -> None:
+        self.type = type
+        self.name = name
+        self.expected = expected
+        self.known = False
+
+    def to_readable(self):
+        d = {'type': self.type, 'name': self.name, 'known': self.known}
+        if self.expected is not None:
+            d['expected'] = self.expected
+        return d
+
+ValidatorProblem.TYPES = [
+    ValidatorProblem.MISSING,
+    ValidatorProblem.NOT_DEFINED,
+    ValidatorProblem.WRONG_TYPE,
+    ValidatorProblem.MISSING_VALIDATION,
+    ValidatorProblem.OPT_MISSING,
+    ValidatorProblem.OPT_NOT_DEFINED,
+]
+
 class ValidatorProfile:
     """
     ValidationProfile define parameters describing how to do the validation against the standard.
@@ -36,6 +68,7 @@ class ValidatorProfile:
         self.prefix = None
         self.standard = None
         self.standard_from = None
+        self.expected = None
 
     @staticmethod
     def create(data):
@@ -44,10 +77,18 @@ class ValidatorProfile:
 
         Example:
         standard:
-          file: 'path/to/standard.json'
-        survey:
-          prefix: 'key prefix to remove '
+          # Full Path 
+          file: 'path/to/standard.json' # Full name of the standard
+          # Or directly from git repo
+          name: 'standard_name' # name of the standard
+          repo: 'influenzanet/surveys-standards' # repo name (optionnal, only if dont want default)
+          revision: 'master' # revision in the repo, optionnal only if not master last commit
+          # OR directly URL where to get the json standard
 
+        prefix: 'key prefix to remove '
+        expected: # Expected differences with the standard
+          question_missing: [ "list of missing questions or pattern like Q10c*..."]
+          question_unknown: [ "list of not expected questions or pattern like Q10c*..."]
 
         """
         p = ValidatorProfile()
@@ -56,7 +97,15 @@ class ValidatorProfile:
             raise ConfigError("standard entry expected")
         std = data['standard']
         p.standard_from = std
+        if 'expected' in data:
+            p.expected = p.load_expected(data['expected'])
         return p
+
+    def load_expected(self, expected:dict)-> Dict:
+        for n, v in expected.items():
+            if not n in ValidatorProblem.TYPES:
+                raise ConfigError("Unknown problem %s in expected" % (n, ))
+        return expected            
 
     def load_standard(self):
         std = self.standard_from
@@ -82,10 +131,11 @@ class ValidatorProfile:
             url = std['url']
         if url is not None:
             standard_json = json.loads(read_from_url(url))
-            print(standard_json)
+            #print(standard_json)
 
         self.standard = json_parser_survey_standard(standard_json)
-            
+
+
 
 COMPATIBLE_TYPES = {
     standard.SINGLE_CHOICE_TYPE : models.RG_TYPE_SINGLE,
@@ -153,7 +203,7 @@ class SurveyStandardValidator:
                 if item_key.startswith(self.profile.prefix):
                     item_key = item_key[len(self.profile.prefix):]
             if not item_key in expected:
-                problems.append({'type': 'not_defined', 'name': item_key})
+                problems.append(ValidatorProblem(ValidatorProblem.NOT_DEFINED, item_key))
                 continue
             std = expected[item_key]
             self.compare_question(item, rg, std, problems)
@@ -161,17 +211,61 @@ class SurveyStandardValidator:
             
         if len(expected) > 0:
             for e in expected:
-                problems.append({'type': 'missing', 'name': e})
+                problems.append(ValidatorProblem(ValidatorProblem.MISSING, e))
+
+        self.validate_problems(problems)
 
         return problems
 
-    def compare_question(self, item:SurveySingleItem, rg, std: StandardQuestion, problems: list):
+    def validate_problems(self, problems:List[ValidatorProblem]):
+        expected = self.profile.expected
+        if expected is None:
+            return
+        for problem in problems:
+            for pb_type in [ValidatorProblem.MISSING_VALIDATION, ValidatorProblem.NOT_DEFINED, ValidatorProblem.MISSING, ValidatorProblem.WRONG_TYPE]:
+                if(problem.type == pb_type):
+                    if self.problem_in_list(expected.get(pb_type, None), problem.name):
+                        problem.known = True
+
+    def problem_in_list(self, knowns, name):
+        if knowns is None:
+            return False
+        for known in knowns:
+            if known.find('*') >= 0:
+                pattern = known.replace('*', '')
+                return name.startswith(pattern)
+            return name == known
+
+    def compare_question(self, item:SurveySingleItem, rg: Optional[ItemDictionnary], std: StandardQuestion, problems: List[ValidatorProblem]):
         expected_type = COMPATIBLE_TYPES.get(std.type, None)
         if expected_type is not None:
             if rg.type != expected_type:
-                problems.append({'type':'wrong_type', 'name':item.key, 'expected': expected_type})
+                problems.append(ValidatorProblem(ValidatorProblem.WRONG_TYPE, item.key, expected_type))
         if std.mandatory:
             if item.validations is None:
-                problems.append({'type':'missing_validation', 'name':item.key})
+                problems.append(ValidatorProblem(ValidatorProblem.MISSING_VALIDATION, item.key))
+        
+        if len(std.responses) == 0:
+            return
+        expected_keys = OrderedDict()
+        for r in std.responses.values():
+            expected_keys[r.value] = r
+        if rg.options is not None:
+            for r in rg.options:
+                if r.item_key in expected_keys:
+                    del expected_keys[r.item_key]
+                else:
+                    problems.append(ValidatorProblem(ValidatorProblem.OPT_NOT_DEFINED, std.data_name + '.' + r.item_key))
+        
+        for name,k in expected_keys.items():
+            problems.append(ValidatorProblem(ValidatorProblem.OPT_MISSING, std.data_name + '.' + name))
+                
+    def filter_known(self, problems: List[ValidatorProblem])->List[ValidatorProblem]:
+        pp = []
+        for p in problems:
+            if p.known:
+                continue
+            pp.append(p)
+        return pp
+            
 
-    
