403f3fbef9ac2c37d76113b4b8cb9a994c7744b9
Clément Turbelin <clement.turbelin@gmail.com>
2022-11-26 18:05:31 +0100

    add incremental export command, refactor download

--- ifncli/commands/response.py
+++ ifncli/commands/response.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 from cliff.command import Command
 from . import register
 import json
-from ..utils import read_yaml
+from ..utils import read_yaml, write_content, read_json
 from typing import Dict,List,Optional
 
 def get_survey_parser_based_on_time(parsers, key: str, ts):
@@ -48,6 +48,9 @@ def export_data(file_name, data):
             f.write(data)
     print("File generated at: {}".format(file_name))
 
+
+ISO_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
 class ExportProfile:
 
     def get_bool(self, data, name, default):
@@ -66,7 +69,7 @@ class ExportProfile:
             if not isinstance(v, str):
                 raise Exception("Field %s must be string" % (name, ))
             if values is not None and v not in values:
-                raise Exception("Invalid value '%s' in %s" % (v, name))
+                raise Exception("Invalid value '%s' in %s, expect: %s" % (v, name, ', '.join(values) ))
         else:
             v = default
         return v
@@ -114,6 +117,135 @@ class ExportProfile:
 
         self.rename_columns = profile.get('rename_columns', None)
 
+        self.start_time = None
+        self.max_time = None
+        if 'start_time' in profile:
+            self.start_time = datetime.strptime(profile['start_time'], ISO_TIME_FORMAT)
+        
+        if 'max_time' in profile:
+            self.max_time = datetime.strptime(profile['max_time'], ISO_TIME_FORMAT)
+        
+
+class ExportCatalog:
+
+    def __init__(self, path:str, start_time:datetime, max_time:datetime):
+        self.file = path + '/catalog.json'
+        self.current_end = start_time
+        self.min_time = start_time
+        self.max_time = max_time
+        if os.path.exists(self.file):
+            self.load()
+        else:
+            self.catalog = []
+    
+    def load(self):
+        data = read_json(self.file)
+        def from_time(time:str):
+            return datetime.stpftime(TIME_FORMAT)
+        self.catalog = []
+        previous_end = None
+        for i, row in data.iteritems():
+            start_time = from_time(row['start'])
+            end_time = from_time(row['end'])
+            self.check_range(start_time, self.min_time, self.max_time, "%d start_time" % (i,) )
+            self.check_range(end_time, self.min_time, self.max_time, "%d end_time" % (i,)  )
+            
+            if previous_end is not None:
+                self.check_range(start_time, previous_end, None, "%d start_time" % (i,))
+            
+            self.current_end = end_time
+            previous_end = end_time
+            self.append(start_time, end_time, row['file'])
+
+    def check_range(self, time:datetime, min_t:datetime, max_t:Optional[datetime], name:str):
+        if max_t is not None and time > max_t:
+            raise Exception("%s (%s) after max (%s)" % (name, time, max_t))
+        if time < min_t:
+            raise Exception("%s (%s) befor max (%s)" % (name, time, min_t))
+            
+
+    def append(self, start_time, end_time, file):
+        self.catalog.append({'start': start_time, 'end': end_time, 'file': file})
+        self.current_end = end_time
+
+    def save(self):
+        def encoder(d):
+            if isinstance(d, datetime):
+                return d.strftime(ISO_TIME_FORMAT)
+        write_content(self.file, json.dumps(self.catalog, default=encoder))
+
+    def get_last_time(self):
+        return self.current_end
+            
+class Exporter:
+
+    def __init__(self, profile:ExportProfile, client, study_key):
+        self.profile = profile
+        self.client = client
+        self.study_key = study_key
+
+    def export(self, start_time: Optional[datetime], end_time: Optional[datetime], output_folder:str):
+        
+        profile = self.profile
+        
+        survey_key = profile.survey_key
+    
+        resp = self.client.get_response_csv(
+            self.study_key, survey_key,
+            profile.key_separator,
+            profile.response_format,
+            profile.short_keys,
+            profile.meta_infos,
+            start_time.timestamp() if start_time is not None else None, 
+            end_time.timestamp() if end_time is not None else None
+        )
+
+        if resp is None:
+            return None
+
+        if not profile.rename_columns is None:
+            resp = replace_columns(profile.rename_columns, resp)        
+
+        os.makedirs(output_folder, exist_ok=True)
+
+        query_range_text = ""
+        if start_time is not None:
+            query_range_text += "_" + start_time.strftime("%Y-%m-%d-%H-%M-%S")
+        if end_time is not None:
+            query_range_text += "_" + end_time.strftime("%Y-%m-%d-%H-%M-%S")
+
+        response_file_name =  "{}_responses{}.{}".format(survey_key, query_range_text, profile.response_extension)
+        
+        export_data(os.path.join(output_folder, response_file_name), resp)
+        return response_file_name
+
+    def export_all(self, output:str):
+        catalog = ExportCatalog(output, self.profile.start_time, self.profile.max_time)
+        output_folder = output + '/' + self.profile.survey_key
+        os.makedirs(output_folder, exist_ok=True)
+        start_time = catalog.get_last_time()
+        already_has_data = False
+        # Max download time, if not provided only load one years (prevent infinite loop)
+        max_time = self.profile.max_time if self.profile.max_time is not None else start_time + timedelta(days=365)
+        loaded = 0
+        while start_time < max_time:
+            end_time = start_time + timedelta(days=6)
+            end_time = end_time.replace(hour=23, minute=59)
+            print("> %s - %s" % (start_time, end_time))
+            r = self.export(start_time, end_time, output_folder)
+            if r is None: 
+                if already_has_data:
+                    break
+            else:
+                already_has_data = True
+                loaded += 1
+                catalog.append(start_time, end_time, r)
+                catalog.save()
+            start_time = start_time + timedelta(days=7)
+        print("%d file(s) loaded" % (loaded))
+            
+
+
 class ResponseDownloader(Command):
     """
         Download responses from a set of survey (not implemented yet)
@@ -136,42 +268,20 @@ class ResponseDownloader(Command):
         query_end_date = args.query_end
         profile = ExportProfile(args.profile)
 
-        query_start_time = datetime.strptime(query_start_date, "%Y-%m-%d-%H-%M-%S").timestamp() if query_start_date is not None else None
-        query_end_time = datetime.strptime(query_end_date, "%Y-%m-%d-%H-%M-%S").timestamp() if query_end_date is not None else None
+        output_folder = "export_" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
         
-        survey_key = profile.survey_key
-
+        query_start_time = datetime.strptime(query_start_date, "%Y-%m-%d-%H-%M-%S") if query_start_date is not None else None
+        query_end_time = datetime.strptime(query_end_date, "%Y-%m-%d-%H-%M-%S") if query_end_date is not None else None
+        
         client = self.app.get_management_api()
             
-        resp = client.get_response_csv(
-            study_key, survey_key,
-            profile.key_separator,
-            profile.response_format,
-            profile.short_keys,
-            profile.meta_infos,
-            query_start_time, query_end_time
-        )
+        exporter = Exporter(profile, client, study_key)
 
-        if resp is None:
+        r = exporter.export(query_start_time, query_end_time, output_folder )
+
+        if r is None:
             print("No files were generated.")
             exit()
-
-        if not profile.rename_columns is None:
-            resp = replace_columns(profile.rename_columns, resp)        
-
-        output_folder = args.output
-        
-        os.makedirs(output_folder)
-
-        query_range_text = ""
-        if query_start_date is not None:
-            query_range_text += "_" + query_start_date
-        if query_end_date is not None:
-            query_range_text += "_" + query_end_date
-
-        response_file_name =  "{}_responses{}.{}".format(survey_key, query_range_text, profile.response_extension)
-        
-        export_data(os.path.join(output_folder, response_file_name), resp)
         
         if profile.survey_info is None:
             return
@@ -180,15 +290,14 @@ class ResponseDownloader(Command):
 
         survey_info_text = ""
         if survey_info['format'] == "csv":
-            survey_info_text = client.get_survey_info_preview_csv(study_key, survey_key, survey_info['lang'], profile.short_keys)
+            survey_info_text = client.get_survey_info_preview_csv(study_key, profile.survey_key, survey_info['lang'], profile.short_keys)
         else:
-            survey_infos = client.get_survey_info_preview(study_key, survey_key, survey_info['lang'], profile.short_keys)
+            survey_infos = client.get_survey_info_preview(study_key, profile.survey_key, survey_info['lang'], profile.short_keys)
             if survey_infos is not None:
                 survey_info_text = json.dumps(survey_infos, indent=2)
 
         info_file_name =  "{}_survey_info.{}".format(survey_key, survey_info['format'])
         export_data(os.path.join(output_folder, info_file_name), survey_info_text) 
-        print("File generated at: {}".format(info_file_name))
 
 class ResponseSchemaDownloader(Command):
     """
@@ -227,5 +336,32 @@ class ResponseSchemaDownloader(Command):
         export_data(os.path.join(output_folder, info_file_name), survey_info_text) 
         print("File generated at: {}".format(info_file_name))
 
+class ResponseExporter(Command):
+    """
+        Export incremental
+    """
+
+    name = "response:export"
+
+    def get_parser(self, prog_name):
+        parser = super(ResponseExporter, self).get_parser(prog_name)
+        parser.add_argument("--profile", help="Export profile yaml file")
+        parser.add_argument("--study", type=str, required=True, help="Study key")
+        parser.add_argument("--output", type=str, help="Output folder")
+        return parser
+        
+    def take_action(self, args):
+        study_key = args.study
+        profile = ExportProfile(args.profile)
+
+        output_folder = args.output
+        
+        client = self.app.get_management_api()
+            
+        exporter = Exporter(profile, client, study_key)
+
+        r = exporter.export_all(output_folder )
+
+register(ResponseExporter)
 register(ResponseSchemaDownloader)
 register(ResponseDownloader)
