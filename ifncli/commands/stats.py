77eea29baa639c2e4899b8aa335030e3363a2a6a
Clément Turbelin <clement.turbelin@gmail.com>
2022-12-28 09:27:16 +0100

    use new counter format (v0.2+)

--- ifncli/commands/stats.py
+++ ifncli/commands/stats.py
@@ -4,7 +4,7 @@ from . import register
 import requests
 from typing import Dict,List
 import json
-from ..utils import Output
+from ..utils import Output,write_content
 from datetime import datetime
 
 class UserStatsCommand(Command):
@@ -15,9 +15,9 @@ class UserStatsCommand(Command):
 
     def get_parser(self, prog_name):
         parser = super(UserStatsCommand, self).get_parser(prog_name)
-       # parser.add_argument("--uri", type=str, help="URI of the service", default="/stats/users")
         parser.add_argument("--file", type=str, required=False, help="File to put the data")
-       # parser.add_argument("--instance", help="Name of the instance")
+        parser.add_argument("--layout", action="store_true", help="Use stats directory layout to put files")
+        
         return parser
 
     def handle_data(self, counters:List):
@@ -27,7 +27,7 @@ class UserStatsCommand(Command):
             if not 'value' in counter or 'error' in counter:
                 continue
             v = counter['value']
-            data[name] = v['value']
+            data[name] = v
         return data
 
     def take_action(self, args):
@@ -42,17 +42,31 @@ class UserStatsCommand(Command):
         conf = cfg['users_stats']
         url = conf['url']
         auth = conf['basic_auth']
-
+        
         url += '/fetch/'+ instance
 
         response = requests.get(url, auth=('stats', auth))
         if response.status_code != 200:
             raise response.raise_for_status()
         
-        out = Output(args.file)
+        now = datetime.now()
+
+        if args.layout:
+            if not 'stats_path' in conf:
+                raise Exception("--layout option requires 'stats_path' in 'users_stats' config entry")
+            stats_path = conf['stats_path']
+            filename = now.strftime("%Y%m%d%H%M")
+            file = "%s/%s.json" % (stats_path, filename)
+        else:
+            file = args.file
+
+        out = Output(file)
 
         data = self.handle_data(response.json())
-        data["_time_"] = datetime.now().isoformat()
+        data["_time_"] = now.isoformat()
         out.write(json.dumps(data))
+        if args.layout:
+            # Write last file
+            write_content("%s/last" % stats_path, filename)
         
 register(UserStatsCommand)
