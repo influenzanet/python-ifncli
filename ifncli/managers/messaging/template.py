2888cb066bb6c022806fe15962f28cd156684674
Clément Turbelin <clement.turbelin@gmail.com>
2023-03-08 19:26:51 +0100

    improve variable processing in email template & checks

--- ifncli/managers/messaging/template.py
+++ ifncli/managers/messaging/template.py
@@ -1,6 +1,16 @@
 from ...platform import PlatformResources
 from ...utils import read_content
-from .utils import read_and_encode_template, wrap_layout,decode_template
+from .utils import read_and_encode_template, wrap_layout, decode_template
+
+from typing import Optional, List
+
+class TemplateResult:
+    def __init__(self, content:str, problems=Optional[List[str]]):
+        self.content = content
+        self.problems = problems
+
+    def has_problems(self):
+        return len(self.problems) > 0
 
 class TemplateLoader:
     """
@@ -28,6 +38,9 @@ class TemplateLoader:
                 url = url[:-1] 
         self.vars = template_vars
 
+        # List of problems in the last template loading
+        self.problems = []
+
     def load(self, template_path, language:str)->str:
         """
             Load a template from file and wrap with layout if available
@@ -36,9 +49,10 @@ class TemplateLoader:
         data['language'] = language
         return read_and_encode_template(template_path, layout=self.layout, vars=data)
 
-    def bind(self, content:str, language:str, is_encoded:bool):
+    def bind(self, content:str, language:str, is_encoded:bool)->TemplateResult:
         data = self.vars.copy()
         data['language'] = language
         if is_encoded:
             content = decode_template(content)
-        return wrap_layout(content, layout=self.layout, vars=data)
+        content, problems = wrap_layout(content, layout=self.layout, vars=data)
+        return TemplateResult(content, problems)
--- ifncli/managers/messaging/utils.py
+++ ifncli/managers/messaging/utils.py
@@ -1,7 +1,7 @@
 import os
-import os
+import re
 import base64
-
+from typing import Optional, Dict
 from ...utils import write_content
 
 def find_template_file(m_type, folder_with_templates):
@@ -28,29 +28,106 @@ def decode_template(encoded:str):
     """
     return base64.b64decode(encoded).decode()
 
-def wrap_layout(content, layout=None, vars=None)->str:
+def wrap_layout(content, layout=None, vars=None):
     """
         Wrap a content with a layout.
         Content is placed into a placeholder in the layout '{=main_content=}'
         Some other variables can be replaced in the layout using the same syntax {=var=} (e.g. title variable the expected placeholder is {=title=} in the layout)
         vars provides the values for each extra variable as a dictionary (key=variable name)
     """
-    if vars is not None:
-        for name, value in vars.items():
-            var = '{=' + name + '=}'
-            content = content.replace(var, value)
-
+    content, errors = bind_content(content, vars)
     if layout is not None:
         content = layout.replace('{=main_content=}', content)
-    return content
+    return content, errors
 
+""""
+Variable syntax regexp {=name=} {= name =}
+"""
+VAR_REGEXP = re.compile("(\{=\s*([-\w]+)\s*=\})", re.IGNORECASE)
+
+def resolve_vars(vars:Optional[Dict]):
+    """"
+        Resolve values and parse reference inside values with circular dependency detection
+        So variables can contains reference to other variables
+    """
+    if vars is None:
+        return (None, None)
+    values = {} # Resolved values
+    temporary = [] # Visiting items
+    marked = [] # Already visited 
+    problems = [] # Collected problems
+    def resolve(name):
+        if name in temporary:
+            raise Exception("Circular dependency :" + "/".join(temporary))
+        if name not in marked:
+            temporary.append(name)
+            if name in vars:
+                value = vars[name]
+                for p in VAR_REGEXP.findall(value):
+                    n = p[1]
+                    resolve(n)
+                value, pp = bind_vars(value, values)
+                if len(pp) > 0:
+                    for p in pp:
+                        problems.append( "%s in '%s'" % (p, name) )
+                values[name] = value
+            else:
+                problems.append("reference '%s' not found at %s " % (name, "/".join(temporary)))
+            marked.append(name)
+            temporary.remove(name)
+    for n in vars.keys():
+        resolve(n)
+    return values, problems
+
+def bind_vars(data:str, vars:Optional[Dict]):
+    """"
+        Bind variables with values in vars in a content string data
+        variables are using the syntax {= name =}
+    """
+    problems = []
+    if vars is None:
+        return data
+    for p in VAR_REGEXP.findall(data):
+        m = p[0]
+        name = p[1]
+        if name in vars:
+            data = data.replace(m, vars[name])
+        else:
+            problems.append("'%s' not found" % name)
+    return data, problems
+
+def bind_content(data, vars):
+    """
+        Bind a string content with variables in vars, the content can contains reference to variables in vars using
+        the {=name=} syntax
+        vars can also contain variables reference (with the same syntax) and are resolved before parsing the content 
+
+        Returns:
+            data : content with variables binded with their resolved values
+            problems: List[str] list of problems detected in content ()
+    """
+    if vars is None:
+        return data
+    problems = []
+    values, pp = resolve_vars(vars)
+    if len(pp) > 0:
+        problems.extend(pp)
+    data, pp = bind_vars(data, values)
+    if len(pp) > 0:
+        problems.extend(pp)
+    return data, problems
 
 def read_and_convert_html(path, vars=None, layout=None):
     content = open(path, 'r', encoding='UTF-8').read()
-    content = wrap_layout(content, layout=layout, vars=vars)
+    content, problems = wrap_layout(content, layout=layout, vars=vars)
     built = vars is not None or layout is not None
     if built:
-        write_content(path + '.built', content)
+        write_content(path + '.built.html', content)
+        if len(problems) > 0:
+            write_content(path + '.built.problems', "\n".join(problems))
+    if len(problems) > 0:
+        for p in problems:
+            print("%s : %s" % (path, p))
     return encode_template(content)
 
 def read_and_encode_template(path, vars=None, layout=None):
