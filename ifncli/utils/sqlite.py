6f7a3aacd91ee8d312ea6ebf503d64c63c6cbdab
Clément Turbelin <clement.turbelin@gmail.com>
2025-03-28 19:02:43 +0100

    add sqlite helper

---
+++ ifncli/utils/sqlite.py
@@ -0,0 +1,53 @@
+import sqlite3
+import os
+from typing import List
+
+
+class DbError(Exception):
+    pass
+
+class SqliteDb:
+    """
+    Simple helper class to manage a little Sqlite database. 
+    It wraps sqlite db to expose simpler API
+    """    
+    def __init__(self, db_path, allow_create=True):
+        db_exists = os.path.exists(db_path)
+        self.db = sqlite3.connect(db_path)
+        if not db_exists and not allow_create:
+            raise DbError("Database '%s' doesnt exists" % (db_path))
+        self.setup(not db_exists)
+
+    def setup(self, empty_db:bool):
+        """
+            This method can be overriden by child class to create the empty db
+            empty_db is True if db has just been created
+        """
+        pass
+    
+    def table_exists(self, table_name):
+        query = "SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'".format(table_name=table_name)
+        r = self.fetch_one(query)
+        return r is not None and len(r) == 1
+
+    def fetch_one(self, query, data=()): 
+        cur = self.db.cursor()
+        res = cur.execute(query, data)
+        return res.fetchone()
+    
+    def fetch_all(self, query, data=()): 
+        cur = self.db.cursor()
+        res = cur.execute(query, data)
+        return res.fetchall()
+    
+    def execute(self, query, data=(), commit=True):
+        cur = self.db.cursor()
+        cur.execute(query, data)
+        if commit:
+            self.db.commit()
+
+    def execute_many(self, query, data:List, commit=True):
+        cur = self.db.cursor()
+        cur.executemany(query, data)
+        if commit:
+            self.db.commit()
