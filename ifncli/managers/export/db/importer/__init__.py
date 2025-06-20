2d35fd762157a8d9d16c6f1f2053bae15c6f7a1c
Clément Turbelin <clement.turbelin@gmail.com>
2025-06-20 18:21:30 +0200

    fix removed import

--- ifncli/managers/export/db/importer/__init__.py
+++ ifncli/managers/export/db/importer/__init__.py
@@ -1,4 +1,4 @@
 from .schema import SurveySchema
 from .version_selector import VersionSelector
-from .importer import Importer, SourceQuery, Writer, DuckDbWriter, ParquerWriter
+from .importer import Importer, SourceQuery, Writer, DuckDbWriter
 from .profile import ImporterProfile
